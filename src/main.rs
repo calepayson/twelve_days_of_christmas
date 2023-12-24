50b53048150c83061032dc2448af0b6b558d58ae
calepayson <calepayson@mac.com>
2023-12-24 13:20:08 -0800

    Implemented an array containing all the daily lines

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 const OPENING_LINES: &str = "On the first day of Christmas,\nmy true love gave to me";
 
+const DAILY_LINES: [&str; 12] = [
+    "And a partridge in a pear tree",
+    "Two turle doves,",
+    "Three French hens,",
+    "Four calling birds,",
+    "Five golden rings,",
+    "Six geese a-laying,",
+    "Seven swans a-swimming,",
+    "Eight maids a-milking,",
+    "Nine ladies dancing,",
+    "Ten lords a-leeping,",
+    "Eleven pipers piping,",
+    "Twelve drummers drumming"
+];
+
 fn main() {
     println!("{OPENING_LINES}");
+    println!("{}", DAILY_LINES[0]);
 }
